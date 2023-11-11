2f60150c8cd2e272d1512366e78a1b69730336da
thanhtam1306 <147906151+thanhtam1306@users.noreply.github.com>
2023-11-11 17:11:46 +0700

    Add files via upload

---
+++ MyOOP/Entity/Product.java
@@ -0,0 +1,97 @@
+package MyOOP.Entity;
+
+public abstract class Product {
+    protected String ID;
+    protected String nameProduct;
+    protected String unit;
+    protected int quantity;
+    protected int price;
+    protected int priceImport;
+    protected boolean isDelete;
+    protected boolean isSearch;
+
+    public Product() {
+        this.ID = "";
+        this.nameProduct = "";
+        this.price = 0;
+        this.unit = "";
+        this.quantity = 0;
+        this.isDelete = true;
+        this.isSearch = true;
+    }
+
+    public Product(int type, String nameProduct, String unit, int quantity, int price, int priceImport) {
+        this.ID = generateIdProduct(type);
+        this.nameProduct = nameProduct;
+        this.unit = unit;
+        this.quantity = quantity;
+        this.price = price;
+        this.priceImport = priceImport;
+        this.isDelete = false;
+        this.isSearch = false;
+        if (quantity <= 0 ) {
+            this.isDelete = true;
+        }
+
+    }
+    public String generateIdProduct(int type) {
+        // 1 = Foods
+        // 2 = Drinks
+        String firstID = "";
+        if (type == 1) {
+            firstID = "FD";
+        }
+        else {
+            firstID = "DK";
+        }
+        return firstID + "-" + (int)(Math.random() * 100000000);
+    }
+
+
+
+    public abstract void print();
+
+    public String getID() {
+        return ID;
+    }
+
+    public boolean getIsDelete() {
+        return isDelete;
+    }
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    public boolean getIsSearch(){
+        return isSearch;
+    }
+
+
+
+    public void setDelete(boolean flag) {
+        isDelete = flag;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPriceImport() {
+        return priceImport;
+    }
+    public void setSearch(boolean flag){
+        isSearch = flag;
+    }
+}
