0ece958867e62f0387902f526f3cd81af90ae4f4
Tran Kim Minh <147906211+trankimminh@users.noreply.github.com>
2023-11-28 22:03:10 +0700

    546

--- Entity/PromotionsSale.java
+++ Entity/PromotionsSale.java
@@ -1,58 +1,164 @@
 package DoAnOOP.Entity;
-
-
 import DoAnOOP.Manager.Validate;
+import DoAnOOP.Entity.ServiceFile;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Arrays;
 
 public class PromotionsSale {
     private String namePromotions;
-    private String codeDiscount;
+    private String idPromotions;
     private String startDate;
     private String endDate;
-    private int moneyDiscount;
 
+    int totalVoucher=0;
+    Voucher[] voucher= new Voucher[totalVoucher];
 
     public PromotionsSale() {
     }
 
-    public PromotionsSale(String namePromotions, String firstKey, String startDate, String endDate, int moneyDiscount) {
+    public PromotionsSale(String namePromotions, String idPromotions, String startDate, String endDate) {
         this.namePromotions = namePromotions;
-        this.codeDiscount = creatCodeDiscount(firstKey);
+        this.idPromotions = idPromotions;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.moneyDiscount = moneyDiscount;
     }
 
-//    public String creatVoucher() {
-//        long discount = new Validate().checkMoneyInput("Nhập vào số tiền giảm giá");
-//        if (discount == -1) {
-//            return null;
-//        }
-//        listVoucher = Arrays.copyOf(listVoucher, totalVoucher + 1);
-//        listVoucher[totalVoucher++] = new Voucher(keyPromotions, discount);
-//        new Validate().clearBuffer();
-//        return listVoucher[totalVoucher - 1].getidVoucher();
-//    }
+    public String getnamePromotions(){
+        return namePromotions;
+    }
+    public void setnamePromotions(String namePromotions){
+        this.namePromotions=namePromotions;
+    }
 
-    public String creatCodeDiscount(String firstKey) {
-        String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz0123456789";
-        int size = 12;
-        StringBuilder stringBuilder = new StringBuilder(size);
-        for(int i = 0; i < size; i++) {
-            int x = (int)(Math.random() * 61);
-            stringBuilder.append(s.charAt(x));
+    public String getidPromotions(){
+        return idPromotions;
+    }
+    public void setidPromotions(String idPromotions){
+        this.idPromotions=idPromotions;
+    }
+
+    public String getstartDate(){
+        return startDate;
+    }
+    public void setstartDate(String startDate){
+        this.startDate=startDate;
+    }
+
+    public String getendDate(){
+        return endDate;
+    }
+    public void setendDate(String endDate){
+        this.endDate=endDate;
+    }
+
+    public String createIdPromotions(){
+        String idPromotions="KM"+"-"+ (int)(Math.random()*10000);
+        return idPromotions;
+    }
+
+    public void input(){
+        idPromotions = createIdPromotions();
+        System.out.println(idPromotions);
+        namePromotions = new Validate().checkStringUser("Nhập tên chương trình khuyến mãi");
+        startDate = new Validate().checkStringUser("Nhập ngày bắt đầu");
+        endDate = new Validate().checkStringUser("Nhập kết thúc");
+        totalVoucher = new Validate().checkNumberInput("Số voucher cần tạo", "Số voucher > 0, ui long nhập lại !");
+        voucher = new Voucher[totalVoucher];
+        for(int i = 0 ; i < totalVoucher ; i++ ){
+            voucher[i]= new Voucher();
+            voucher[i].input();
         }
-        return firstKey + "-" + stringBuilder.toString();
     }
 
-    public void showPromotionsSale() {
-        int colSpace = 25;
-        System.out.printf("%-" + colSpace + "s %-"
-                + colSpace + "s %-"
-                + colSpace + "s %-"
-                + colSpace + "d\n", namePromotions, startDate, endDate, moneyDiscount);
-
+    public void print(){
+        System.out.println("\n-------------KHUYẾN MÃI "+namePromotions+" ("+idPromotions+") từ "+startDate+" đến "+endDate+" -----------");
+        for(int i=0;i<totalVoucher;i++){
+            System.out.println("\nVoucher thứ  "+(i+1));
+            voucher[i].print();
+        }
     }
 
+    public void checkIdVoucher(String idVoucher){
+        //
+    }
+
+    public void addVoucher(){
+        int quantityVoucher = new Validate().checkNumberInput("So voucher muon them", "So phia >0, vui long nhap lai !");
+        for(int i = 0 ; i < quantityVoucher ; i++) {
+        voucher = Arrays.copyOf(voucher, totalVoucher+1);
+                voucher[totalVoucher]=new Voucher();
+                voucher[totalVoucher].input();
+                totalVoucher++;
+        }
+        System.out.println("Thêm voucher thành công !");
+    }
+
+    public void deleteVoucher(){
+        int count = 0;
+        String idVoucherUser=new Validate().checkStringUser("Nhập mã voucher cần xóa của CTKM "+namePromotions);
+        for(int i=0;i<totalVoucher;i++){
+            if((voucher[i].getidVoucher()).equals(idVoucherUser)){
+                totalVoucher--;
+               for(int j=i; j<totalVoucher; j++){
+			   voucher[j] = voucher[j+1];
+               }
+               count++;
+               System.out.println("Xóa voucher thành công !");
+            }
+        }
+        if(count==0){
+            System.out.println("Không tìm thấy voucher !");
+        }
+    }
+
+    public void findVoucher(){
+        int count=0;
+        String idVoucherUser=new Validate().checkStringUser("\nNhập mã voucher cần tìm");
+        for(int i=0;i<totalVoucher;i++){
+            if((voucher[i].getidVoucher()).equals(idVoucherUser)){
+                voucher[i].print();
+                count++;
+            }
+        }
+        if(count==0){
+            System.out.println("Không tìm thấy voucher !");
+        }
+    }
+
+    public void insertVoucher(String idVoucher, int moneyDiscount) {
+        voucher = Arrays.copyOf(voucher, totalVoucher + 1);
+        voucher[totalVoucher] = new Voucher(idVoucher,moneyDiscount);
+        totalVoucher ++;
+    }
+
+    public String printToFile() {
+        String result = "";
+        for (Voucher x : voucher) {
+            result += namePromotions + "|" + idPromotions + "|" + startDate + "|" + endDate + "|" + x.printToFile();
+        }
+        return result;
+    }
+
+    public void readData() {
+        try {
+            FileReader fileReader = new FileReader("Voucher.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = "";
+            while(true){
+                line = bufferedReader.readLine();
+                if(line == null){
+                    break;
+                }
+                voucher = Arrays.copyOf(voucher, totalVoucher++ );
+                String[] split = line.split("\\|");
+                String idVoucher = split[4];
+                int moneyDiscount = Integer.parseInt(split[5]);
+                voucher[totalVoucher++] = new Voucher(idVoucher,moneyDiscount);
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+        }
+    }
 }
