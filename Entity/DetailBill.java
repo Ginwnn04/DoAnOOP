f853cc7fea324b7c41a315aa13c02dfbadd5e119
Tran Kim Minh <147906211+trankimminh@users.noreply.github.com>
2023-11-28 22:12:04 +0700

    xyz

--- Entity/DetailBill.java
+++ Entity/DetailBill.java
@@ -1,71 +1,103 @@
-//package DoAnOOP.Entity;
-//
-//import DoAnOOP.Manager.Validate;
-//
-//import java.io.DataOutputStream;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//
-//public class DetailBill {
-//    String idProduct;
-//    int quantity;
-//
-//    public DetailBill(){
-//        this.idProduct="";
-//        this.quantity=0;
-//    }
-//
-//    public DetailBill(String idProduct, int quantity){
-//        this.idProduct=idProduct;
-//        this.quantity=quantity;
-//     }
-//
-//    public String getidProduct(){
-//        return idProduct;
-//    }
-//    public void setidProduct(String idProduct){
-//        this.idProduct=idProduct;
-//    }
-//
-//    public int getquantity(){
-//        return quantity;
-//    }
-//    public void setquantity(int quantity){
-//        this.quantity=quantity;
-//    }
-//
-//    public float price(ListProduct listProduct) {
-//        float price=0;
-//        if (idProduct != null && !idProduct.isEmpty()) {
-//            Product matchedProduct = listProduct.getVoucherByCode(idProduct);
-//            if (matcheProduct != null) {
-//                price = matchedProduct.getprice();
-//            }
-//        }
-//        return price;
-//    }
-//
-//    public String nameProduct(ListProduct listProduct) {
-//        String nameProduct="";
-//        if (idProduct != null && !idProduct.isEmpty()) {
-//            Product matchedProduct = listProduct.getVoucherByCode(idProduct);
-//            if (matcheProduct != null) {
-//                nameProduct = matchedProduct.getnameProduct();
-//            }
-//        }
-//        return nameProduct;
-//    }
-//
-//    public void input(){
-//        idProduct=new Validate().checkStringUser("Nhập mã sản phẩm");
-//        quantity=new Validate().checkIntUser("Nhập số lượng sản phẩm");
-//    }
-//
-//    public void print(ListProduct listProduct){
-//        System.out.println(idProduct+" | "+nameProduct(listProduct)+" | "+quantity+" | "+total(listProduct));
-//    }
-//
-//    public float total(ListProduct listProduct){
-//        return quantity*price(listProduct);
-//    }
-//}
+package DoAnOOP.Entity;
+import DoAnOOP.Manager.Validate;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class DetailBill {
+    String idProduct;
+    String nameProduct;
+    int price;
+    int quantity;
+    int quantityCheck;
+    int total;
+
+    public DetailBill(){
+        this.idProduct="";
+        this.nameProduct="";
+        this.price=0;
+        this.quantity=0;
+        this.total=0;
+    }
+
+    public DetailBill(String nameProduct,String idProduct,int price, int quantity, int total ){
+        this.idProduct=idProduct;
+        this.nameProduct=nameProduct;
+        this.price=price;
+        this.quantity=quantity;
+        this.total=total;
+     }
+
+    public String getidProduct(){
+        return idProduct;
+    }
+    public void setidProduct(String idProduct){
+        this.idProduct=idProduct;
+    }
+
+    public int getquantity(){
+        return quantity;
+    }
+    public void setquantity(int quantity){
+        this.quantity=quantity;
+    }
+
+   public String getnameProduct(){
+        return nameProduct;
+    }
+    public void setnameProduct(String nameProduct){
+        this.nameProduct=nameProduct;
+    }
+
+    public int getprice(){
+        return price;
+    }
+    public void setprice(int price){
+        this.price=price;
+    }
+    public int gettotal(){
+        return total;
+    }
+    public void settotal(int total){
+        this.total=total;
+    }
+    public void input(){
+        idProduct=new Validate().checkStringUser("Nhập mã sản phẩm");
+        quantity=new Validate().checkNumberInput("Nhập số lượng sản phẩm","Số lượng sản phẩm > 0, vui lòng nhập lại !");
+        readData();
+        if(quantity>quantityCheck){
+            System.out.println("Sản phẩm trong kho không đủ !");
+        }
+        total = price*quantity;
+    }
+
+    public void print(){
+        System.out.println(idProduct+"|"+nameProduct+"|"+price+"|"+quantity+"|"+total);
+    }
+
+    public String printToFile() {
+        return nameProduct + "|"+ idProduct + "|" + price + "|" + quantity + "|" + total;
+    }
+
+    public void readData(){
+        try {
+            FileReader fileReader = new FileReader("KhoSanPham.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = "";
+            while((line = bufferedReader.readLine()) != null) {
+                String[] split =  line.split("\\|");
+                String idProductFile = split[0];
+                String nameProductFile = split[1];
+                int quantityFile = Integer.parseInt(split[3]);
+                int priceFile = Integer.parseInt(split[5]);
+                if(idProductFile.equals(idProduct)){
+                    nameProduct = nameProductFile;
+                    price = priceFile;
+                    quantityCheck = quantityFile;
+                }
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+        }
+    }
+}
